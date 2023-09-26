6a8debcf595642d196ae1387ff7e427c218beb68
Lucas Lima <lucas.lima.psico@hotmail.com>
2023-09-26 19:31:01 -0300

    feat: add random name generate function

---
+++ src/database/clientNames.ts
@@ -0,0 +1,47 @@
+const ANIMALS: Record<number, string> = {
+    1: 'Gato',
+    2: 'Cachorro',
+    3: 'Elefante',
+    4: 'Leão',
+    5: 'Tigre',
+    6: 'Girafa', //
+    7: 'Zebra',
+    8: 'Cavalo',
+    9: 'Coelho',
+    10: 'Panda',
+    11: 'Gorila',
+    12: 'Pinguim',
+    13: 'Urso Polar',
+    14: 'Hipopótamo',
+    15: 'Camelo',
+    16: 'Macaco',
+    17: 'Cobra', //
+    18: 'Rinoceronte',
+    19: 'Pássaro',
+    20: 'Peixe',
+}
+
+const COLORS: Record<number, string> = {
+    1: 'Vermelho',
+    2: 'Verde',
+    3: 'Azul',
+    4: 'Amarelo',
+    5: 'Laranja',
+    6: 'Roxo',
+    7: 'Rosa',
+    8: 'Marrom',
+    9: 'Preto',
+    10: 'Branco',
+    11: 'Cinza',
+    12: 'Ciano',
+    13: 'Magenta',
+    14: 'Dourado',
+    15: 'Prateado',
+    16: 'Turquesa',
+    17: 'Lavanda',
+    18: 'Azeitona',
+    19: 'Bege',
+    20: 'Aqua',
+}
+
+export { ANIMALS, COLORS }
---
+++ src/utils/randomChatName.ts
@@ -0,0 +1,18 @@
+import { CONNECTEDCLIENT } from '../database/connectedClients'
+import { ANIMALS, COLORS } from '../database/clientNames'
+
+export function randomNameGenerate(): string {
+    let randomName: string
+
+    do {
+        const randomAnimalNumber: number = Math.floor(Math.random() * 20) + 1
+        const randomColorNumber: number = Math.floor(Math.random() * 20) + 1
+
+        const randomAnimal: string = ANIMALS[randomAnimalNumber]
+        const randomColor: string = COLORS[randomColorNumber]
+
+        randomName = `${randomAnimal} ${randomColor}`
+    } while (CONNECTEDCLIENT.some((cliente) => cliente.author === randomName))
+
+    return randomName
+}
