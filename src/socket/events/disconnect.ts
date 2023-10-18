8815c3a361130aeb998ccccd690ae41904fe962b
Lucas Lima <lucas.lima.psico@hotmail.com>
2023-10-18 20:58:18 -0300

    fix: remove extra function

--- src/socket/events/disconnect.ts
+++ src/socket/events/disconnect.ts
@@ -3,8 +3,7 @@ import { createServerMessage } from '../../utils/serverMessage'
 import { Socket } from 'socket.io'
 
 export function disconnect(socket: Socket, authorName: string) {
-    createServerMessage(`O ${authorName} saiu do chat`)
     const disconnectAuthor = CONNECTED_AUTHORS.findIndex((author) => author.authorId === socket.id)
     CONNECTED_AUTHORS.splice(disconnectAuthor, 1)
-    return
+    return { CONNECTED_AUTHORS }
 }
