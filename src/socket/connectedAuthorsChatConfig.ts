49b3df1d64ba3cc35a1c7f64f3f0b8ca2aced466
Lucas Lima <lucas.lima.psico@hotmail.com>
2023-10-18 20:57:42 -0300

    fix: configureConnectedAuthors

--- src/socket/connectedAuthorsChatConfig.ts
+++ src/socket/connectedAuthorsChatConfig.ts
@@ -5,7 +5,7 @@ export const configureConnectedAuthors = (ioChat: any) => {
     ioChat.on('connection', (socket: Socket) => {
         // retorna pro usuário a lista de clientes conectados
         socket.on('chat_clients', () => {
-            socket.emit('connected_clients', CONNECTED_AUTHORS)
+            ioChat.emit('connected_clients', CONNECTED_AUTHORS)
         })
     })
 
