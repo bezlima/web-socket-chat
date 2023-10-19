4422a9cacb5b22655c68b7b48ebbd7c521cb9c16
Lucas Lima <lucas.lima.psico@hotmail.com>
2023-10-18 23:02:31 -0300

    feat: message status validate

--- src/socket/events/updateMessage.ts
+++ src/socket/events/updateMessage.ts
@@ -10,18 +10,23 @@ export function updateMessage(socket: Socket, data: IData) {
     if (!data.author) {
         console.error('Erro: Nome de usuário não definido para o socket.')
         return
+    } else {
+        const messageIndex = CHAT.findIndex(
+            (message) => message.authorId === socket.id && message.id === data.messageId
+        )
+
+        if (messageIndex === -1) {
+            console.error('Erro: Mensagem não encontrada ou você não tem permissão para atualizá-la.')
+            return {}
+        } else if (CHAT[messageIndex].status === 2) {
+            console.error('Erro: Mensagens excluídas não podem ser deletadas')
+            return {}
+        } else {
+            CHAT[messageIndex].text = data.newText
+            CHAT[messageIndex].status = 1
+            const updatedMessage = CHAT[messageIndex]
+
+            return updatedMessage
+        }
     }
-
-    const messageIndex = CHAT.findIndex((message) => message.authorId === socket.id && message.id === data.messageId)
-
-    if (messageIndex === -1) {
-        console.error('Erro: Mensagem não encontrada ou você não tem permissão para atualizá-la.')
-        return {}
-    }
-
-    CHAT[messageIndex].text = data.newText
-    CHAT[messageIndex].status = 1
-    const updatedMessage = CHAT[messageIndex]
-
-    return updatedMessage
 }
